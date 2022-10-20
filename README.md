4b78ef8db537851cf89e78ce81ad3a4c91a41d57
Olly Squires <osquires@wealthsimple.com>
2022-10-20 14:37:02 -0400

    add usage tool

--- README.md
+++ README.md
@@ -8,13 +8,30 @@ Given a CSV of LD flag keys, this tool will iterate through the list and archive
 git clone https://github.com/theChumpus/ld-flag-archiver.git
 cd ld-flag-archiver
 yarn install
+```
 
+## How to use
+
+### Check Flag Usage
+
+Before you can search for references to the flag in the WS codebase, you'll need to generate a new GH access token.
+
+1. In GH, go to Settings > Developer settings > Personal access tokens > Tokens (classic) > Generate new token (classic)
+2. Generate a new access token with `repo` access. Remember to authorize with SSO!
+
+```
+export GITHUB_ACCESS_TOKEN=<insert API token here>
+
+yarn usage <path to CSV>
+```
+
+### Archive flags
+
+To actually archive flags, you'll need to generate a LaunchDarkly API token.
+
+```
 export LD_API_TOKEN=<insert API token here>
-```
 
-## How to run
-
-```
 yarn archive <path to CSV>
 ```
 
---
+++ src/usage.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const eachSeries = require("async/eachSeries");
+const { parseCsv } = require("./csv-parser");
+
+const codeSearchUrl = "https://api.github.com/search/code";
+
+function search(flagKey) {
+  return axios
+    .get(`${codeSearchUrl}?q=${encodeURIComponent(flagKey)}+org:wealthsimple`, {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    })
+    .then((response) => {
+      const items = response.data.items;
+
+      if (items.length > 0) {
+        console.log(
+          `✅ [${flagKey}] No references found in code. Should be safe to archive!`
+        );
+      } else {
+        console.log(
+          `❗️ [${flagKey}] References found in code. Remove references before archiving!`
+        );
+      }
+    })
+    .catch((err) => {
+      console.error(err.response.data);
+    });
+}
+
+function run() {
+  if (process.env.GITHUB_ACCESS_TOKEN === undefined) {
+    console.error("Please set GITHUB_ACCESS_TOKEN environment variable first.");
+    process.exit(1);
+  }
+
+  parseCsv(process.argv[2])
+    .then((flags) => {
+      console.log(`🔎  Searching usage of ${flags.length} flags`);
+
+      const iteratee = (flagKey, callback) => {
+        return search(flagKey).then(() => callback());
+      };
+
+      return eachSeries(flags, iteratee);
+    })
+    .then(() => {
+      process.exit();
+    });
+}
+
+run();
